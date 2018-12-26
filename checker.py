6e6ce52ec5aab8e9b8c1f1849caa764b127d3b06
memento_code <bigbuddalive@gmail.com>
2018-12-26 11:35:30 +0200

    mod2

--- checker.py
+++ checker.py
@@ -1,5 +1,7 @@
 import socket
 import bitstream
+import time
+import binascii
 
 IN_UDP_PORT = 5001
 out_udp_port = 5000
@@ -8,10 +10,7 @@ in_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 in_sock.bind(('127.0.0.1', IN_UDP_PORT))
 out_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # out_sock.bind(('127.0.0.1', out_udp_port))
-host = '127.0.0.1'
-port = 5001
-addr = (host, port)
-# i=0
+
 buf = []
 
 access_code = [b'\x00', b'\x00', b'\x00', b'\x01',
@@ -23,29 +22,42 @@ access_code = [b'\x00', b'\x00', b'\x00', b'\x01',
                b'\x00', b'\x00', b'\x00', b'\x01',
                b'\x01', b'\x01', b'\x00', b'\x01']
 while True:
-    if len(buf) < 32:
-        # print(len(access_code))
+    tstart = time.time()
+    if len(buf) < len(access_code):
         nf_buf_data = in_sock.recv(1)
         buf.append(nf_buf_data)
-        # continue
-    elif len(buf) == 32 and buf != access_code:
+        tcheck1 = time.time()
+        # print(tcheck1 - tstart)
+    elif len(buf) == len(access_code) and buf != access_code:
         buf.pop(0)
         f_buf_data = in_sock.recv(1)
         buf.append(f_buf_data)
-        # continue
+        tcheck2 = time.time()
     elif buf == access_code:
-        print('locked')
-        buf = []
-        for i in range(480):
+        # print('locked')
+        for i in range(508):
             out_data = []
+            tloop1 = time.time()
+            # print(tstart - tloop1)
             for i in range(8):
                 stream.write(in_sock.recv(1))
                 data = list(str(stream.read(8)))
                 new_data = list(data[7])
                 out_data.extend(new_data)
                 if len(out_data) == 8:
+                    bytetime = time.time()
                     transfer_data = ''.join(out_data)
-                    print(hex(int(transfer_data,2)))
+                    transfer_data = str(hex(int(transfer_data, 2)))
+                    transfer_data = transfer_data[2:]
+                    if len(transfer_data) < 2:
+                        transfer_data = transfer_data + '0'
+                    transfer_data = str(binascii.unhexlify(transfer_data))
+                    print(transfer_data[2])
+                    printime = time.time()
+                    print(printime - bytetime)
+                    # print(hex(int(transfer_data,2)))
                     # import to file
-                    #with open("recovered.txt", "a",) as f:
+                    # with open("recovered.txt", "a",) as f:
                     #    f.write(transfer_data.decode("utf-8") )
+
+        buf = []
